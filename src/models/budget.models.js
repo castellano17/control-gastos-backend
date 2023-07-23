760ee5c4f31c9cefae0fb636f5d7ef09b9ebf465
Esmir Roque Castellano <74161172+castellano17@users.noreply.github.com>
2023-07-23 17:49:18 -0600

    Update budget.models.js

--- src/models/budget.models.js
+++ src/models/budget.models.js
@@ -1,53 +1,24 @@
-const Budget = require("../models/budget.models");
-const uuid = require("uuid");
+const { DataTypes } = require("sequelize");
+const db = require("../utils/database");
 
-const findBudgetUser = async (id) => {
-  // console.log(id);
-  const data = await Budget.findOne({
-    where: {
-      user_id: id, // buscar en la columna user_id
+const Users = require("./users.models");
+
+const Budget = db.define("budgets", {
+  id: {
+    type: DataTypes.UUID,
+    primaryKey: true,
+  },
+  total: {
+    type: DataTypes.DECIMAL,
+    allowNull: false,
+  },
+  userId: {
+    type: DataTypes.UUID,
+    allowNull: false,
+    references: {
+      model: Users,
+      key: "id",
     },
-    attributes: {
-      exclude: ["createdAt", "updatedAt", "userId"],
-    },
-  });
-  return data;
-};
-
-const createbudget = async (budgetObject, userId) => {
-  const existingBudget = await Budget.findOne({ where: { userId } });
-  if (existingBudget) {
-    throw new Error("A budget already exists for this user");
-  }
-
-  if (!budgetObject.total || Number(budgetObject.total) <= 0) {
-    throw { message: "total is required and must be a positive number" };
-  }
-
-  const newBudget = {
-    id: uuid.v4(),
-    total: Number(budgetObject.total),
-    userId: userId,
-  };
-  const data = await Budget.create(newBudget);
-  return data;
-};
-
-const updateBudget = async (id, budgetObj) => {
-  const selectedBudget = await Budget.findOne({
-    where: {
-      id: id,
-    },
-  });
-
-  if (!selectedBudget) return null;
-
-  const modifiedBudget = await selectedBudget.update(budgetObj);
-  return modifiedBudget;
-};
-
-module.exports = {
-  findBudgetUser,
-  createbudget,
-  updateBudget,
-};
+  },
+});
+module.exports = Budget;
